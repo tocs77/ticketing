0cdafb65d157c6bdb36a0aacac233931c1dd1247
Ilya Golubkov <ilya.golubkov@gmail.com>
2023-03-06 20:09:07 +0600

    Purchase ticket client page

--- client/pages/tickets/[ticketId].js
+++ client/pages/tickets/[ticketId].js
@@ -1,5 +1,31 @@
-const TicketInfo = ({ tickedId }) => {
-  return <div>Ticket info {tickedId}</div>;
+import { useRequest } from '../../hooks/use-request';
+
+const TicketInfo = ({ ticket }) => {
+  const [createOrderRequest, createOrderError] = useRequest({
+    url: '/api/orders',
+    method: 'post',
+    body: { ticketId: ticket.id },
+    onSuccess: (order) => {
+      console.log('Did order', order);
+    },
+  });
+  return (
+    <div>
+      <h2>{ticket.title}</h2>
+      <h4>Price: ${ticket.price}</h4>
+      <button className='btn btn-primary' onClick={createOrderRequest}>
+        Purchase
+      </button>
+      {createOrderError}
+    </div>
+  );
+};
+
+TicketInfo.getInitialProps = async (context, client) => {
+  const { ticketId } = context.query;
+  console.log(context.query);
+  const { data } = await client.get(`/api/tickets/${ticketId}`);
+  return { ticket: data };
 };
 
 export default TicketInfo;
--- tickets/src/routes/show.ts
+++ tickets/src/routes/show.ts
@@ -1,15 +1,23 @@
 import express from 'express';
-import { NotFoundError } from '@tocstick/common';
+import { NotFoundError, BadRequestError } from '@tocstick/common';
 import { Ticket } from '../models/ticket';
 
 const router = express.Router();
 
 router.get('/:id', async (req, res, next) => {
-  const ticket = await Ticket.findById(req.params.id);
-  if (!ticket) {
-    return next(new NotFoundError());
+  if (!req.params.id) {
+    return next(new BadRequestError(`Invalid ticket id ${req.params.id}`));
   }
-  res.status(200).send(ticket);
+
+  try {
+    const ticket = await Ticket.findById(req.params.id);
+    if (!ticket) {
+      return next(new NotFoundError());
+    }
+    res.status(200).send(ticket);
+  } catch (error) {
+    return next(new BadRequestError(`Invalid ticket id ${error}`));
+  }
 });
 
 export { router as getTicketRouter };
